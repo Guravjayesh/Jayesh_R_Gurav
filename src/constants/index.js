0226230c3346ad0dff3c5ba54942d580aea9b3be
Jayesh Gurav <jayeshgurav46@gmail.com>
2023-09-18 16:32:10 +0530

    resume

--- src/constants/index.js
+++ src/constants/index.js
@@ -413,6 +413,7 @@ import {
         },
         
       ],
+      // image: "https://www.geotab.com/CMS-Media-production/Blog/NA/_2020/Aug/reverse-parking/blog-why-reverse-parking-hero@2x.jpg",
       image: "https://www.geotab.com/CMS-Media-production/Blog/NA/_2020/Aug/reverse-parking/blog-why-reverse-parking-hero@2x.jpg",
       source_code_link: "https://github.com/Guravjayesh/",
     },
